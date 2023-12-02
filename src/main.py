8a732ead61852ccfae3e84a25f1244e32be8452b
takyafumin <takyafumin@gmail.com>
2023-12-02 20:53:34 +0900

    pythonプログラムに引数受け取りを追加

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from datetime import datetime
 from zoneinfo import ZoneInfo
@@ -5,8 +6,13 @@ from zoneinfo import ZoneInfo
 # timezone
 tz_tokyo = ZoneInfo("Asia/Tokyo")
 
+# 引数参照
+args = sys.argv
+
 now = datetime.now(tz_tokyo)
 print(f"ジョブ開始時間:{now.strftime('%x %X')}")
+if 1 < len(args):
+    print(f"引数:{args[1]}")
 
 # 5秒待機
 time.sleep(5)
